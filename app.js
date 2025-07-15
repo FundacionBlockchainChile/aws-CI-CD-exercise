03f7bf59c6866b0571bef336cd47317ff5b12e57
Sergio Castro <scastrof@gmail.com>
2025-07-14 22:18:27 -0400

    test: Actualizado mensaje de bienvenida para probar el pipeline

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from AWS CI/CD Pipeline!');
+  res.send('¡Hola! Esta es una prueba del pipeline CI/CD en AWS. La aplicación se ha desplegado correctamente.');
 });
 
 app.listen(port, () => {
